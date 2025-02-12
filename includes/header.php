b360d176fda91c903e46ff9a2c5711865da8378f
Samkit <samkitjain2809@gmail.com>
2025-02-12 21:46:39 +0530

    Refactor header links to use BASE_URL constant for improved maintainability

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,10 @@
+<?php
+define("BASE_URL", "http://localhost/Sphatik-v1/");
+?>
+
 <header class="header">
     <div class="container">
-        <a href="../index.php" class="logo">Sphatik</a>
+        <a href="sphatik-v1/index.php" class="logo">Sphatik</a>
         <nav class="nav">
             <button class="mobile-menu-btn">
                 <span></span>
@@ -8,15 +12,15 @@
                 <span></span>
             </button>
             <ul class="nav-list">
-                <li class="lsit-item"><a href="./index.php">Home</a></li>
-                <li class="lsit-item"><a href="./courses/courses.php">Courses</a></li>
-                <li class="lsit-item"><a href="./freelance.php">Freelance</a></li>
-                <li class="lsit-item"><a href="./services.php">Local Services</a></li>
+                <li class="lsit-item"><a href="<?= BASE_URL ?>index.php">Home</a></li>
+                <li class="lsit-item"><a href="<?= BASE_URL ?>courses/courses.php">Courses</a></li>
+                <li class="lsit-item"><a href="<?= BASE_URL ?>freelance.php">Freelance</a></li>
+                <li class="lsit-item"><a href="<?= BASE_URL ?>services.php">Local Services</a></li>
                 <?php if (isset($_SESSION['user_id'])): ?>
-                    <li><a href="profile.php">Profile</a></li>
-                    <li><a href="auth/logout.php">Logout</a></li>
+                    <li class="lsit-item"><a href="<?= BASE_URL ?>profile.php">Profile</a></li>
+                    <li class="lsit-item"><a href="<?= BASE_URL ?>/auth/logout.php">Logout</a></li>
                 <?php else: ?>
-                    <li><a href="auth/login.php" class="btn btn-login">Login</a></li>
+                    <li class="lsit-item"><a href="<?= BASE_URL ?>/auth/login.php" class="btn btn-login">Login</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
