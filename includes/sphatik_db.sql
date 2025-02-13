0b1690e985b1df5cf66517a1e0dec87bd00e075a
Mann-Makhecha <mannmakhecha1506@gmail.com>
2025-02-13 09:21:11 +0530

    Refactor course display to use dynamic generation from database and add course card function

--- includes/sphatik_db.sql
+++ includes/sphatik_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2025 at 02:55 PM
+-- Generation Time: Feb 13, 2025 at 04:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `sphatik_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `img_src` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `enroll_link` varchar(255) DEFAULT NULL,
+  `college_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `img_src`, `title`, `description`, `enroll_link`, `college_name`) VALUES
+(1, '../includes/images/dcn.jpg', 'Data Communication and Networking', 'Short description of the course.', 'dcn.php', 'College Name'),
+(2, '../includes/images/dbms.jpg', 'Database Management System', 'Short description of the course.', '#', 'College Name'),
+(3, '../includes/images/mit.png', 'Microprocessor and Interfacing Techniques', 'Short description of the course.', 'mit.php', 'College Name'),
+(4, '../includes/images/os.jpg', 'Operating System', 'Short description of the course.', 'course-details.php?id=2', 'College Name'),
+(5, '../includes/images/python.jpg', 'Python Programming', 'Short description of the course.', 'course-details.php?id=2', 'College Name'),
+(6, '../includes/images/ds.jpg', 'Data Structures', 'Short description of the course.', 'course-details.php?id=2', 'College Name'),
+(7, '../includes/images/se.jpg', 'Software Engineering', 'Short description of the course.', 'course-details.php?id=2', 'College Name'),
+(8, '../includes/images/wt.jpg', 'Web Technologies', 'Short description of the course.', 'course-details.php?id=2', 'College Name'),
+(9, '../includes/images/ict.jpg', 'Information and Communication Technologies', 'Short description of the course.', 'course-details.php?id=2', 'College Name');
 
 -- --------------------------------------------------------
 
@@ -82,7 +112,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `phone`, `password_hash`, `role`, `verified`, `created_at`) VALUES
 (1, 'dhiru', 'dhiru12@mail.com', '', '$2y$10$jAtLcoiR./z5UEsn3EHnoeSUz7fTXVamZYTd7zzaTh/VXLFmyjO6q', '', 0, '2025-02-12 03:51:49'),
-(7, 'kishu', 'krish12@mail.com', '', '$2y$10$Z.4ULlM4T9IHQA.J9QKHDejL6rgDsog9Erx0hEh4rA6OFEQT91pui', 'delivery_partner', 0, '2025-02-12 04:35:25');
+(7, 'kishu', 'krish12@mail.com', '', '$2y$10$Z.4ULlM4T9IHQA.J9QKHDejL6rgDsog9Erx0hEh4rA6OFEQT91pui', 'delivery_partner', 0, '2025-02-12 04:35:25'),
+(8, 'hello', 'hello@mail.com', '', '$2y$10$L490hRCx6EeuYqvFfhTbKO2Y9DqCwVSPhqIEulEU7J/I5bR6RZOZ2', '', 0, '2025-02-12 16:52:01'),
+(9, 'acb', 'abc@mail.com', '', '$2y$10$4Sr4YETP0o5D5CGQs9lDbeB2r9OgPE6dgIlD796Dp5YCuJRFJRrl.', 'delivery_partner', 0, '2025-02-12 16:58:11'),
+(10, 'qwe', 'qwe@mail.com', '', '$2y$10$axDT5GaNk/aRD2UXwNCPuegS7F9m7TG5KFUjTj.tBXA1.PoNBFqTa', '', 0, '2025-02-12 16:59:08');
 
 -- --------------------------------------------------------
 
@@ -103,11 +136,20 @@ CREATE TABLE `user_profiles` (
 
 INSERT INTO `user_profiles` (`profile_id`, `user_id`, `full_name`, `phone`) VALUES
 (1, 1, 'Dhiraj Jagwani', '977795522'),
-(2, 7, 'Krish Jain', '8200700139');
+(2, 7, 'Krish Jain', '8200700139'),
+(3, 8, 'mannn', '8564213955'),
+(4, 9, 'abc', '9876543210'),
+(5, 10, 'qwe', '0123654789');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `delivery_partners`
@@ -144,10 +186,16 @@ ALTER TABLE `user_profiles`
 --
 
 --
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `delivery_partners`
 --
 ALTER TABLE `delivery_partners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -159,13 +207,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_profiles`
 --
 ALTER TABLE `user_profiles`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
