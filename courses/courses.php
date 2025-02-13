d7cdd5de4efa27d159638c5eed04d79068f1d985
Mann-Makhecha <mannmakhecha1506@gmail.com>
2025-02-13 10:46:26 +0530

    hi

--- courses/courses.php
+++ courses/courses.php
@@ -1,4 +1,25 @@
-<!-- filepath: /c:/xampp/htdocs/Sphatik v1/courses.php -->
+<?php
+include '../config/database.php';
+
+// Fetch courses from database
+$sql = "SELECT img_src, title, description, enroll_link, college_name FROM courses";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$courses = $stmt->fetchAll();
+
+// Function to generate course card markup
+function generateCourseCard($img, $title, $desc, $link, $college) {
+    return "<div class='course-card'>
+                <img src='$img' alt='$title'>
+                <h3>$title</h3>
+                <p>$desc</p>
+                <p class='college-name'><strong>Offered by:</strong> $college</p>
+                <a href='$link' class='btn'>Enroll Now</a>
+            </div>";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,31 +31,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
-    <?php include '../includes/header.php'; 
-    include '../config/database.php';
-    include '../includes/functions.php';
-    ?>
-
+    <?php include '../includes/header.php'; ?>
 
     <main class="courses-page">
         <div class="container">
             <h1>Our Courses</h1>
             <div class="courses-grid">
-                <?php
-                $sql = "SELECT img_src, title, description, enroll_link, college_name FROM courses";
-                $stmt = $pdo->prepare($sql);
-                $stmt->execute();
-                $courses = $stmt->fetchAll();
-
-                if ($courses) {
-                    foreach ($courses as $course) {
-                        echo generateCourseCard($course['img_src'], $course['title'], $course['description'], $course['enroll_link'], $course['college_name']);
-                    }
-                } else {
-                    echo "<p>No courses available.</p>";
+            <?php
+            if ($courses) {
+                foreach ($courses as $course) {
+                    echo generateCourseCard($course['img_src'], $course['title'], $course['description'], $course['enroll_link'], $course['college_name']);
                 }
-        ?>
-                <!-- Add more course cards as needed -->
+            } else {
+                echo "<p>No courses available.</p>";
+            }
+            ?>
             </div>
         </div>
     </main>
